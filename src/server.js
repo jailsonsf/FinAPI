7eceaa02522e7f29c2518c544b1a45f68126f244
jailsonsf <jailson.s.f@outlook.com>
2022-02-25 20:54:36 -0300

    Add route to create account

--- src/server.js
+++ src/server.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const res = require('express/lib/response');
+const { v4: uuidV4 } = require('uuid');
+
 
 const app = express();
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    return res.send('ok');
+const customers = []
+/**
+ * cpf - String
+ * name - String
+ * id - uuid
+ * statement - []
+ */
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already Exists" });
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidV4(),
+        statement: []
+    });
+
+    return response.status(201).send();
 });
 
 app.listen(3333);

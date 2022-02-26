881dd35e1e131f9d2401cc6aa12f5619ebd827b3
jailsonsf <jailson.s.f@outlook.com>
2022-02-26 11:23:23 -0300

    Create route to withdraw

--- src/server.js
+++ src/server.js
@@ -20,6 +20,18 @@ function verifyIfExistsAccountCpf(request, response, next) {
     next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 const customers = []
 /**
  * cpf - String
@@ -64,6 +76,27 @@ app.post('/deposit', verifyIfExistsAccountCpf, (request, response) => {
         amount,
         created_at: new Date(),
         type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCpf, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
     }
 
     customer.statement.push(statementOperation);

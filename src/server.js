4fa3c5015da077c6c81611945561bb3ed4a0f9d6
jailsonsf <jailson.s.f@outlook.com>
2022-02-26 11:11:46 -0300

    Add middleware to verify  account and create route to deposit

--- src/server.js
+++ src/server.js
@@ -6,6 +6,20 @@ const app = express();
 app.use(express.json());
 
 
+function verifyIfExistsAccountCpf(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    }
+
+    request.customer = customer;
+
+    next();
+}
+
 const customers = []
 /**
  * cpf - String
@@ -34,16 +48,27 @@ app.post('/account', (request, response) => {
     return response.status(201).send();
 });
 
-app.get('/statement/:cpf', (request, response) => {
-    const { cpf } = request.params;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if (!customer) {
-        return response.status(400).json({ error: "Customer not found" });
-    }
+app.get('/statement', verifyIfExistsAccountCpf, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCpf, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 });
 
 app.listen(3333);
